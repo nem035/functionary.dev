ea3f799bcc693c8121a06ae0602c1a7407ade683
Nemanja Stojanovic <n.stojanovic035@gmail.com>
2026-08-16 13:26:06 -0400

    Deploy Functionary to Cloudflare

--- worker/index.ts
+++ worker/index.ts
@@ -29,6 +29,11 @@ const worker = {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
 
+    if (url.hostname === "www.functionary.dev") {
+      url.hostname = "functionary.dev";
+      return Response.redirect(url, 308);
+    }
+
     if (url.pathname === "/_vinext/image") {
       const allowedWidths = [...DEFAULT_DEVICE_SIZES, ...DEFAULT_IMAGE_SIZES];
       return handleImageOptimization(request, {
